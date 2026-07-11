6c72c72c5c40a33c33bd8fc21c95e3559bec7f65
Trunal7778 <145747931+Trunal7778@users.noreply.github.com>
2026-07-11 13:22:54 +0530

    first commit

---
+++ Test1/student/views.py
@@ -0,0 +1,26 @@
+# from urllib import request
+
+from urllib import request
+
+from django.shortcuts import render
+
+#from django.http import HttpResponse
+# Create your views here.
+
+#def home(request):
+#return HttpResponse("Hello, Trunal prajapati welcome to django world !")  
+
+def home(request): 
+    data = {
+     'name': 'Trunal Prajapati',
+     'course': 'Django',
+     'collage': 'JG UNIVERSITY' 
+    } 
+    subject = ['python','Django','agile','angular', 'Big data']
+    return render(request, 'index.html',{'data': data, 'subject': subject})
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'About.html')
