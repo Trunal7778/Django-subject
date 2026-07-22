403e0d86599960799b73fa904ba8d16f60897035
Trunal7778 <145747931+Trunal7778@users.noreply.github.com>
2026-07-22 12:33:37 +0530

    new admin created and coursemodel added

--- Test1/student/models.py
+++ Test1/student/models.py
@@ -10,4 +10,16 @@ class Student(models.Model):
     mobile = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.name 
+        return self.name  
+    
+class Course(models.Model):
+    
+    courseName = models.CharField(max_length=100)
+    courseCode=models.CharField(max_length=50)
+    startDate=models.DateField()
+    endDate=models.DateField()
+    facultyName=models.CharField(max_length=100)
+    isActive=models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.courseName
